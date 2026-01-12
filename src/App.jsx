a600087302bb0a3db5df26edf7b950d7cd4fa880
alessandraciccone <alessandraciccone@icloud.com>
2026-01-12 21:18:31 +0100

    adds import tailwinda

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h2 className="bg-red-400 font-mono text-4xl text-gray-200 m-4 text-center">
+        Hello World
+      </h2>
+    </>
+  );
 }
 
 export default App;
